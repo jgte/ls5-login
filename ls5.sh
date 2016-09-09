4a54ff846ec5b6e882e0ba2e71afc5f98d3c55c8
jgte <j.g.deteixeiradaencarnacao@tudelft.nl>
2016-09-09 18:00:07 -0500

    added the possibility to run non-interative commands remotely; added the possibility for password-less logins (zero-interation); added the possbility to change directory remotely

--- ls5.sh
+++ ls5.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 #define here the secret file
-SECRET_FILE=<some file with the 32 character secret string>
+SECRET_FILE=<some file with a valid QR secret>
 
 #define here the username
 USER_NOW=<some valid username>
+
+#define here the address of ls5
+LS5_ADDRESS=ls5.tacc.utexas.edu
 
 function machine_is
 {
@@ -32,43 +35,77 @@ then
   npm install --global authenticator-cli
 fi
 
-if [[ ! "$@" == "${@/debug/}" ]] || [[ ! "$@" == "${@/echo/}" ]]
+#defaults
+CSR=
+ECHO=
+SSHKEY_FILE=
+REMOTE_COM="exec $SHELL -l"
+
+#propagate input arguments
+for i in "$@"
+do
+  case $i in
+  debug|echo)
+    ECHO='echo '
+  ;;
+  csr)
+    CSR='login3.'
+  ;;
+  key=*)
+    SSHKEY_FILE="-i ${i#key=}"
+  ;;
+  dir=*)
+    REMOTE_COM="cd ${i#dir=}; $REMOTE_COM"
+  ;;
+  com=*)
+    REMOTE_COM="${i#com=}"
+  ;;
+  esac
+done
+
+#retrieve token
+TOKEN=$(authenticator --key $(cat $SECRET_FILE) | grep Token | awk '{print $2}')
+if [[ ! "$@" == "${@/token/}" ]]
 then
-  ECHO='echo '
-else
-  ECHO=''
+  if machine_is Darwin
+  then
+    echo $TOKEN | pbcopy
+  elif machine is Ubuntu
+  then
+    if which xclip &> /dev/null
+    then
+      echo $TOKEN | xclip -selection clipboard
+    else
+      echo "Please run 'sudo apt-get install xclip'"
+    fi
+  else
+    echo "ERROR: cannot copy token to the clipboard, this OS is not supported."
+  fi
+  echo "Copied to clipboard the token $TOKEN"
+  exit
 fi
 
-if [[ ! "$@" == "${@/csr/}" ]]
-then
-  CSR='login3.'
-else
-  CSR=''
-fi
-
-if [ -z "$ECHO" ]
+if [ -z "$ECHO" ] && [ -z "$SSHKEY_FILE" ]
 then
   #retrieve password
   read -p "Password for user $USER_NOW:" -s PASSWD
   echo ""
+  PASSWD_EXPECT="expect \"Password: \"
+send \"$PASSWD\r\""
 else
-  PASSWD="fake password"
+  PASSWD_EXPECT=
 fi
-
-#retrieve token
-TOKEN=$(authenticator --key $(cat $SECRET_FILE) | grep Token | awk '{print $2}')
-echo "Token is: $TOKEN"
 
 #wait a little bit
 WAIT_TOKEN=0.1
 
 echo "Logging in (please wait, this takes a couple of seconds):"
 $ECHO expect -c "
-  spawn ssh -l $USER_NOW -Y ${CSR}ls5.tacc.utexas.edu
-  expect \"Password: \"
-  send \"$PASSWD\r\"
-  expect \"TACC Token: \"
-  sleep $WAIT_TOKEN
-  send \"$TOKEN\r\"
-  interact
+spawn ssh -l $USER_NOW $SSHKEY_FILE -Y -t $CSR$LS5_ADDRESS \"$REMOTE_COM\"
+$PASSWD_EXPECT
+expect \"TACC Token: \"
+sleep $WAIT_TOKEN
+send \"$TOKEN\r\"
+interact
 " || exit $?
+
